3d8b848b8148c806f39d18f79406f4975759bd04
airyland <airyland@qq.com>
2013-08-14 15:30:34 +0800

    只有一个元素时不进行不必要操作

--- shuffle.js
+++ shuffle.js
@@ -4,6 +4,9 @@
 (function(jQuery) {
 	jQuery.fn.shuffle = function(c) {
 		c = [];
+		// 如果只有一个元素就不进行无谓操作了
+		if(this.length <=1 ) return this;
+		
 		return this.each(function() {
 			c.push(jQuery(this).clone(true))
 		}).each(function(a, b) {
